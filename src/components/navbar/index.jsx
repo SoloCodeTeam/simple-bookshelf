9046415a031e205134f3d72cb40e93c00a62cc9e
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-05-26 22:57:34 +0500

    navbar & modal

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,56 @@
-import "./style.css"
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import { colors } from '@mui/material';
 
 export function NavbarComponent() {
-    return(
-        <div className="Navbar">
-            <h1>BookShelf</h1>
-            <button>add</button>
-            <form className="modal">
-                <h2>New Book!</h2>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-            </form>
+    const [modal, setModal] = useState(false);
+    const modalOpen = () => setModal(true);
+    const modalClose = () => setModal(false);
+
+    return (
+        <div>
+            <AppBar position="static" sx={{backgroundColor: "#EEF7FF",borderBottom: '1px solid #615EFC',backdropFilter: 'none',}}>
+                <Toolbar>
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1, color: "#615EFC" }}>
+                        BookShelf
+                    </Typography>
+                    <Button variant="contained" sx={{color:"#EEF7FF",backgroundColor:"#615EFC"}} onClick={modalOpen}>add</Button>
+                </Toolbar>
+            </AppBar>
+            <Dialog open={modal} onClose={modalClose}>
+                <DialogTitle>Add a new book</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To add a new book to your bookshelf, please enter the book details here.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Book Name"
+                        type="text"
+                        fullWidth
+                        variant="outlined"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={modalClose} sx={{color:"#615EFC"}}>
+                        cancel
+                    </Button>
+                    <Button onClick={modalClose} sx={{color:"#615EFC"}}>
+                        add
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
-    )
+    );
 }
