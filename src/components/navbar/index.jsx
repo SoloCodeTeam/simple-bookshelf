e006eb6bb7054a8648acbdfe4c11a833ca0697c2
Ibrohimov <ibrohimovabdulloh573@gmail.com>
2024-05-26 22:15:21 +0500

    navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,6 +5,12 @@ export function NavbarComponent() {
         <div className="Navbar">
             <h1>BookShelf</h1>
             <button>add</button>
+            <form className="modal">
+                <h2>New Book!</h2>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+            </form>
         </div>
     )
 }
